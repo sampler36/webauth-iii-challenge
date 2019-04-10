0794f9252ec93d06d0452b064689bb0c1843936f
Talent Antonio <antonio.talenty@gmail.com>
2019-04-10 16:03:25 +0100

    database and configeration

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const server = require('./api/server.js');
 
 const port = process.env.PORT || 4400;
-server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
+server.listen(port, () => console.log(`\n** Running on port http://localhost:4400  ${port} **\n`));
