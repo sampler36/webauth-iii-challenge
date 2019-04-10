8ef90dea7badf092b1b0baba0fcc7f9b172e4899
Talent Antonio <antonio.talenty@gmail.com>
2019-04-10 17:47:12 +0100

    table update

--- database/migrations/20190410143813_createUsersTable.js
+++ database/migrations/20190410143813_createUsersTable.js
@@ -1,14 +1,14 @@
 exports.up = function(knex) {
-    return knex.schema.createTable('users', users => {
-      users.increments();
+    return knex.schema.createTable('users', (tbl) => {
+      tbl.increments();
   
-      users
+      tbl
         .string('username', 128)
         .notNullable()
         .unique();
-      users.string('password', 128).notNullable();
+      tbl.string('password', 128).notNullable();
 
-      users.string('departments', 526)
+      tbl.string('departments', 526)
            .notNullable()
     });
   };
